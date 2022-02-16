4632cdc4ec4accdd6a89dfc40b14a26ceb4d5c51
Kécia Santos <keciakaroline@gmail.com>
2022-02-16 18:08:32 +0100

    Added React Router

--- src/App.js
+++ src/App.js
@@ -1,58 +1,24 @@
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Homepage";
+import Projects from "./components/Projects";
+import Navbar from "./components/Navbar";
+import ExampleProjects from "./components/ExampleProjects";
 
 function App() {
   return (
     <div className="App">
-      <main>
-        <Homepage />
-      </main>
-      <footer className="App-footer">
-        This project was coded by{" "}
-        <a
-          href="https://www.linkedin.com/in/keciasantos/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Kécia Santos
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/keciasantos/react-portfolio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>{" "}
-        and hosted on{" "}
-        <a
-          href="https://www.netlify.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Netlify.
-        </a>
-        <div class="social-link">
-          <span>
-            <a
-              href="https://github.com/keciasantos"
-              target="_blank"
-              title="GitHub page - Kécia Santos"
-              alt="GitHub page"
-            >
-              <i class="fab fa-github-square"></i>
-            </a>{" "}
-            <a
-              href="https://www.linkedin.com/in/keciasantos/"
-              target="_blank"
-              title="Linkedin page - Kécia Santos"
-              alt="Linkedin page"
-            >
-              <i class="fab fa-linkedin"></i>
-            </a>
-          </span>
-        </div>
-      </footer>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Homepage />}></Route>
+        <Route exact path="/projects" element={<Projects />}></Route>
+        <Route
+          exact
+          path="/exampleprojects"
+          element={<ExampleProjects />}
+        ></Route>
+        {/* <Route path="/artigos" element={Artigos} /> */}
+      </Routes>
     </div>
   );
 }
