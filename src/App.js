92cad8540492e4c0f1f88f9a702a6ab706e8bbfd
Kécia Santos <keciakaroline@gmail.com>
2022-02-17 19:41:07 +0100

    Added ScrollToTop solution

--- src/App.js
+++ src/App.js
@@ -1,27 +1,25 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Homepage";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
-import ExampleProjects from "./components/ExampleProjects";
 import About from "./components/About";
 import Contact from "./components/Contact";
+
+import ScrollToTop from "./ScrollToTop";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Homepage />}></Route>
-        <Route exact path="/projects" element={<Projects />}></Route>
-        <Route
-          exact
-          path="/exampleprojects#homepage"
-          element={<ExampleProjects />}
-        ></Route>
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/contact" element={<Contact />} />
-      </Routes>
+      <ScrollToTop>
+        <Routes>
+          <Route exact path="/" element={<Homepage />}></Route>
+          <Route exact path="/projects" element={<Projects />}></Route>
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+      </ScrollToTop>
     </div>
   );
 }
