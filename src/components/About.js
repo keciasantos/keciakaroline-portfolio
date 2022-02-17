818ef791a4e3170e0c621e6829fe4ad4fcff2f80
Kécia Santos <keciakaroline@gmail.com>
2022-02-17 19:40:49 +0100

    Update profile picture and about me

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,11 @@ import react from "react";
 import Footer from "./Footer";
 import FooterContact from "./FooterContact";
 import "./About.css";
+import profile_2 from "../images/profile_2.jpg";
 
 export default function About() {
   return (
-    <div className="About">
+    <div className="About" id="about">
       <div className="content-container container-fluid">
         <div className="row">
           <div className="col-lg-6">
@@ -14,13 +15,18 @@ export default function About() {
               <h2 className="mt-4">Front-end Developer, based in Europe</h2>
               <p>
                 I am a Materials Engineer from Brazil, living in Germany, that
-                fell in love for the WebDeveloper world. I like challenges and I
-                always give my best to them, adapting when it is necessary. I
-                also believe that teamwork contributes to archive better
-                results.
-                <br />
+                fell in love for the WebDeveloper Front-End world. I like
+                challenges and I always give my best to them, adapting and
+                asking for help when it is necessary. I also believe that
+                teamwork contributes to archive better results, if you don't
+                know how to do something, you can ask for some help and maybe
+                someone has a better ideia or 'brainstorm'.
+                <br />I just finished the SheCodes Workshops, all my
+                certificates are avaliable on Linkedin, you can also check my
+                projects here. <br />
                 I’m currently seeking new opportunities in front-end
-                development. I’m open to work remotely or on site, I have
+                development, to learn more and have some experience in this
+                amazing world. I’m open to work remotely or on site, I have
                 flexible hours. I have skills in HTML, CSS, JavaScript and React
                 as you can see from my projects.
               </p>
@@ -37,7 +43,7 @@ export default function About() {
           </div>
           <div className="col-lg-6">
             <img
-              src="/"
+              src={profile_2}
               className="img-fluid profile"
               alt="Kécia Santos"
               title="Kécia Santos Profile Photo"
