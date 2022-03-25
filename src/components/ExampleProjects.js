d0e149d0c5203d14f698e53ea11da0a81cac399c
Kécia Santos <keciakaroline@gmail.com>
2022-03-25 19:43:39 +0100

    Added new project page

--- src/components/ExampleProjects.js
+++ src/components/ExampleProjects.js
@@ -2,6 +2,7 @@ import react from "react";
 import project_1 from "../images/project_1.png";
 import project_2 from "../images/project_2.png";
 import project_3 from "../images/project_3.png";
+import project_4 from "../images/project_4.jpg";
 import "./ExampleProjects.css";
 
 export default function ExampleProjects() {
@@ -26,6 +27,7 @@ export default function ExampleProjects() {
                   with love (one day I will be a proud mom of a Corgi). It was
                   made after a few weeks of classes about HTML and CSS basic.
                 </p>
+
                 <a
                   href="https://www.shecodes.io/workshops/shecodes-basics-00f05685-1a07-4d9f-a4e1-7b822eb75fcb/projects/503334"
                   className="btn btn-branding-outline"
@@ -104,6 +106,42 @@ export default function ExampleProjects() {
                   View project <i className="fas fa-long-arrow-alt-right"></i>
                 </a>
               </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col">
+              <div className="project-description">
+                <h2 className="mb-3">TODO App</h2>
+                <p className="mb-3">
+                  This project is from{" "}
+                  <a
+                    href="https://www.frontendmentor.io/"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    frontendmentor.io
+                  </a>
+                  , build with React <i className="fab fa-react"></i>. For me it
+                  was a big challenge to apply all this resources in an APP with
+                  so less field experience. Really loved! <br />
+                </p>
+                <a
+                  href="https://todo-app-keciasantos.netlify.app/"
+                  className="btn btn-branding-outline"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  View project <i className="fas fa-long-arrow-alt-right"></i>
+                </a>
+              </div>
+            </div>
+            <div className="col d-none d-lg-block">
+              <img
+                src={project_4}
+                alt="project FrontEndMentor TODO-LIST"
+                className="img-fluid project"
+                title="This is my TODO list project"
+              />
             </div>
           </div>
         </div>
