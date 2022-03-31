12a2d55eb779f5e2c0d8040c82857a0004543682
Kécia Santos <keciakaroline@gmail.com>
2022-03-31 14:45:17 +0200

    Changed style for contact area

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,4 +1,4 @@
-import react from "react";
+import React from "react";
 import Footer from "./Footer";
 import "./Contact.css";
 import ContactForm from "./ContactForm";
@@ -7,66 +7,53 @@ export default function Contact() {
   return (
     <div className="Contact" id="contact">
       <div className="content-container container-fluid">
-        <div className="content-contact ">
-          <h1>Let's get in touch!</h1>
-          <div className="row">
-            <div className="col-md-6 mt-5 mb-5">
-              <div className="row contact">
-                <div className="col-md-6 mt-3 mb-3">
-                  <div className="contact-link">
-                    <h3>Email:</h3>
-                    <a href="mailto:keciakaroline@gmail.com" alt="email link">
-                      keciakaroline@gmail.com
-                    </a>
-                  </div>
-                </div>
-                <div className="col-md-6 mt-3 mb-3">
-                  <div className="contact-link">
-                    <h3>XING:</h3>
-                    <a
-                      href="https://www.xing.com/profile/KeciaKaroline_RolimdosSantos"
-                      alt="XING page"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      Kécia Santos
-                    </a>
-                  </div>
-                </div>
-              </div>
-
-              <div className="row contact">
-                <div className="col-md-6 mt-3 mb-3">
-                  <div className="mt-5 mb-5 contact-link">
-                    <h3>Linkedin:</h3>
-                    <a
-                      href="https://www.linkedin.com/in/keciasantos/"
-                      alt="Linkedin page"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      keciasantos
-                    </a>
-                  </div>
-                </div>
-                <div className="col-md-6 mt-3 mb-3">
-                  <div className="mt-5 mb-5 contact-link">
-                    <h3>GitHub:</h3>
-                    <a
-                      href="https://github.com/keciasantos"
-                      alt="GitHub page"
-                      target="_blank"
-                      rel="noreferrer"
-                    >
-                      keciasantos
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-6 mt-5 mb-5">
-              <ContactForm />
-            </div>
+        <h1>Let's get in touch!</h1>
+        <div className="row">
+          <div className="col-md-6 mt-5 mb-5 text-center">
+            <ul>
+              <li className="contact-link">
+                <h3>Email:</h3>
+                <a href="mailto:keciakaroline@gmail.com" alt="email link">
+                  keciakaroline@gmail.com
+                </a>
+              </li>
+              <li className="contact-link">
+                <h3>XING:</h3>
+                <a
+                  href="https://www.xing.com/profile/KeciaKaroline_RolimdosSantos"
+                  alt="XING page"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Kécia Santos
+                </a>
+              </li>
+              <li className="contact-link">
+                <h3>Linkedin:</h3>
+                <a
+                  href="https://www.linkedin.com/in/keciasantos/"
+                  alt="Linkedin page"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  Kécia Santos
+                </a>
+              </li>
+              <li className="contact-link">
+                <h3>GitHub:</h3>
+                <a
+                  href="https://github.com/keciasantos"
+                  alt="GitHub page"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  keciasantos
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="col-md-6 mt-5 mb-5">
+            <ContactForm />
           </div>
         </div>
       </div>
