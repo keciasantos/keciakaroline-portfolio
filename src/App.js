dca41032a71f8b951c1ec19b103f5d93cda1f12f
Kécia Santos <keciakaroline@gmail.com>
2022-05-11 21:12:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Homepage from "./components/Homepage";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
 import Contact from "./components/Contact";
@@ -15,7 +15,7 @@ function App() {
       <ScrollToTop>
         <Routes>
           <Route exact path="/" element={<Homepage />}></Route>
-          <Route exact path="/projects" element={<Projects />}></Route>
+          {/* <Route exact path="/projects" element={<Projects />}></Route> */}
           <Route exact path="/about" element={<About />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
