cd22a67b517a80ec93616af4f57f0185140843c2
Kécia Santos <keciakaroline@gmail.com>
2022-05-12 17:48:53 +0200

    Update icons

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -6,50 +6,43 @@ export default function SocialLinks() {
   return (
     <>
       <div className="SocialLinks">
-        <ul>
-          <li>
-            <a
-              href="mailto:keciakaroline@gmail.com"
-              rel="noopener noreferrer"
-              alt="email link"
-              target="_blank"
-            >
-              <i className="fa-solid fa-envelope"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://github.com/keciasantos"
-              target="_blank"
-              title="GitHub page - Kécia Santos"
-              alt="GitHub page"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-github-square"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.linkedin.com/in/keciasantos/"
-              target="_blank"
-              title="Linkedin page - Kécia Santos"
-              alt="Linkedin page"
-              rel="noopener noreferrer"
-            >
-              <i className="fa-brands fa-linkedin"></i>
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.xing.com/profile/KeciaKaroline_RolimdosSantos"
-              alt="XING page"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <i className="fa-brands fa-xing-square"></i>
-            </a>
-          </li>
-        </ul>
+        <a
+          href="mailto:keciakaroline@gmail.com"
+          rel="noopener noreferrer"
+          alt="email link"
+          target="_blank"
+        >
+          <i className="fa-solid fa-envelope"></i>
+        </a>
+
+        <a
+          href="https://github.com/keciasantos"
+          target="_blank"
+          title="GitHub page - Kécia Santos"
+          alt="GitHub page"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-github-square"></i>
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/keciasantos/"
+          target="_blank"
+          title="Linkedin page - Kécia Santos"
+          alt="Linkedin page"
+          rel="noopener noreferrer"
+        >
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a
+          href="https://www.xing.com/profile/KeciaKaroline_RolimdosSantos"
+          alt="XING page"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="fa-brands fa-xing-square"></i>
+        </a>
       </div>
     </>
   );
