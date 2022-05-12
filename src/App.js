efc38b0891002c35ed2b8ebbf586b9373c005bbf
Kécia Santos <keciakaroline@gmail.com>
2022-05-12 17:31:42 +0200

    update Navbar visual

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import Homepage from "./components/Homepage";
 import Projects from "./components/Projects";
 import Navbar from "./components/Navbar";
 import About from "./components/About";
-import Contact from "./components/Contact";
-
+import ContactForm from "./components/ContactForm";
 import ScrollToTop from "./ScrollToTop";
 
 function App() {
@@ -16,8 +15,8 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />}></Route>
           <Route exact path="/projects" element={<Projects />}></Route>
-          <Route exact path="/about" element={<About />} />
-          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/about" element={<About />}></Route>
+          <Route exact path="/contact" element={<ContactForm />}></Route>
         </Routes>
       </ScrollToTop>
     </div>
