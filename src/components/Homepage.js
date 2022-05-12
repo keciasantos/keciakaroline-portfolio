30a32659e9adce526b4210e10f06d075a837227a
Kécia Santos <keciakaroline@gmail.com>
2022-05-12 17:31:52 +0200

    Update Homepage visual

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,34 +6,48 @@ import "./Homepage.css";
 import Footer from "./Footer";
 import { HashLink } from "react-router-hash-link";
 import About from "./About";
+import SocialLinks from "./SocialLinks";
 
 export default function Homepage() {
   return (
     <div className="Homepage">
-      <div className="heading">
+      <section className="heading text-center">
         <div className="container-fluid">
-          <div className="row">
-            <div className="col introdution">
-              <h1>Hey, I'm Kécia Santos</h1>
-              <h3 className="font-text mb-5">
-                A Frontend focused Web Developer building the Frontend of
-                Websites and Web Applications
-              </h3>
-
-              <HashLink to="#projects" className="btn btn-branding">
-                Projects
-              </HashLink>
-            </div>
-            <div className="col d-none d-lg-block">
-              <img src={profile_1} alt="Kécia Santos" className="img-fluid" />
+          <div>
+            <img src={profile_1} alt="Kécia Santos" className="img-fluid" />
+          </div>
+          <div className="intro">
+            <h2>Hey, I'm </h2>
+            <h1>Kécia Santos</h1>
+            <h3>
+              A Frontend focused on Web Developer building the Frontend of
+              Websites and Web Applications
+            </h3>
+            <HashLink to="#projects" className="btn btn-branding">
+              Projects
+            </HashLink>
+          </div>
+          <div className="scroll-downs">
+            <div className="mousey">
+              <div className="scroller"></div>
             </div>
           </div>
         </div>
-      </div>
-      <About />
-      <Projects />
-      <ContactForm />
-      <Footer />
+
+        <SocialLinks />
+      </section>
+      <section>
+        <About />
+      </section>
+      <section>
+        <Projects />
+      </section>
+      <section>
+        <ContactForm />
+      </section>
+      <section>
+        <Footer />
+      </section>
     </div>
   );
 }
