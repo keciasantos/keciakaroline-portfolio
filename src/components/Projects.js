1d41ad6500dd9769b868ce601c383ef487b8354f
Kécia Santos <keciakaroline@gmail.com>
2022-05-12 20:12:15 +0200

    update project style

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,127 +1,120 @@
 import React from "react";
-// import project_1 from "../images/project_1.png";
-import project_2 from "../images/project_2.png";
-import project_3 from "../images/project_3.png";
-import project_4 from "../images/project_4.png";
-import project_5 from "../images/project_5.png";
 import "./Projects.css";
 
 export default function Projects() {
   return (
-    <>
-      <div className="Projects" id="projects">
-        <div className="container-fluid">
-          <h1>PROJECTS</h1>
-          <h4>Here you will find my personal and clients projects.</h4>
-          <div className="row">
-            <div className="col-md-6">
-              <h2 className="mb-3">Weather App</h2>
-              <img
-                src={project_2}
-                alt="project weather"
-                className="img-fluid project"
-                title="This is my weather project"
-              />
-              <div className="btn btn-branding">
-                <a href="/" target="_blank" rel="noreferrer">
-                  GitHub <i className="fas fa-long-arrow-alt-right"></i>
-                </a>
-              </div>
-              <div className="btn btn-branding">
-                <a
-                  href="https://react-weather-project-keciasantos.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Demo live <i className="fas fa-long-arrow-alt-right"></i>
-                </a>
-              </div>
-            </div>
+    <div className="Projects" id="projects">
+      <div className="container-fluid">
+        <h1>PROJECTS</h1>
+        <h4>Here you will find my personal and clients projects.</h4>
+        <ul className="project-out">
+          <li className="project">
+            <h2 className="mb-3">Weather App</h2>
+            <a
+              href="../images/project_2.png"
+              alt="project weather"
+              className="img-fluid"
+            ></a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              GitHub <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+            <a
+              href="https://react-weather-project-keciasantos.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              Demo live <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+          </li>
+          <li className="project">
+            <h2 className="mb-3">Dictionary App</h2>
+            <a
+              href="../images/project_3.png"
+              alt="project dictionary"
+              className="img-fluid"
+            ></a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              GitHub <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+            <a
+              href="https://react-dictionary-project.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              Demo live <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+          </li>
+          <li className="project">
+            <h2 className="mb-3">TODO App</h2>
+            <a
+              href="../images/project_4.png"
+              alt="project FrontEndMentor TODO-LIST"
+              className="img-fluid"
+            ></a>
 
-            <div className="col-md-6">
-              <h2 className="mb-3">Dictionary App</h2>
-              <img
-                src={project_3}
-                alt="project dictionary"
-                className="img-fluid project"
-                title="This is my dictionary project"
-              />
-              <div className="btn btn-branding">
-                <a href="/" target="_blank" rel="noreferrer">
-                  GitHub <i className="fas fa-long-arrow-alt-right"></i>
-                </a>
-              </div>
-              <div className="btn btn-branding">
-                <a
-                  href="https://react-dictionary-project.netlify.app/"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  Demo live <i className="fas fa-long-arrow-alt-right"></i>
-                </a>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-md-6">
-                <h2 className="mb-3">TODO App</h2>
-                <img
-                  src={project_4}
-                  alt="project FrontEndMentor TODO-LIST"
-                  className="img-fluid project"
-                  title="This is my TODO list project"
-                />
-                <div className="btn btn-branding">
-                  <a href="/" target="_blank" rel="noreferrer">
-                    GitHub <i className="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-                <div className="btn btn-branding">
-                  <a
-                    href="https://todo-app-keciasantos.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Demo live <i className="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-                <div className="btn btn-branding">
-                  <a
-                    href="https://www.frontendmentor.io/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    frontendmentor.io
-                  </a>
-                </div>
-              </div>
-
-              <div className="col-md-6">
-                <h2 className="mb-3">Portfolio - Yasmin Rolim</h2>
-                <img
-                  src={project_5}
-                  alt="project Yasmin Rolim"
-                  className="img-fluid project"
-                  title="Yasmin Rolim Portfolio"
-                />
-                <div className="btn btn-branding">
-                  <a href="/" target="_blank" rel="noreferrer">
-                    GitHub <i className="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-                <div className="btn btn-branding">
-                  <a
-                    href="https://yasminrolim-arquitetura.netlify.app/"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-                    Demo live <i className="fas fa-long-arrow-alt-right"></i>
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              GitHub <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+            <a
+              href="https://todo-app-keciasantos.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              Demo live <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+            <a
+              href="https://www.frontendmentor.io/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              FrontEnd-Mentor
+            </a>
+          </li>
+          <li className="project">
+            <h2 className="mb-3">Portfolio - Yasmin Rolim</h2>
+            <a
+              href="../images/project_5.png"
+              alt="project Yasmin Rolim"
+              className="img-fluid"
+            ></a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              GitHub <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+            <a
+              href="https://yasminrolim-arquitetura.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="btn btn-branding"
+            >
+              Demo live <i className="fas fa-long-arrow-alt-right"></i>
+            </a>
+          </li>
+        </ul>
       </div>
-    </>
+    </div>
   );
 }
